3075bc32d5bb453a4de859eaaec9bef09fd92c2d
Justin <36967116+jcx975@users.noreply.github.com>
2019-11-05 10:23:15 -0600

    clean data script

---
+++ data/clean_echocardiogram.R
@@ -0,0 +1,14 @@
+
+library(dplyr)
+
+HeartData <- read.csv( "echocardiogram.csv" )
+
+(
+  HeartData
+  %>%select( -c( mult, name, group, alive.at.1 ) )
+  %>%filter( !( survival < 12 & still.alive == 1 ) )
+  %>%mutate( target = case_when( survival >= 12 & still.alive == 1 ~ 1, TRUE ~ 0 ) )
+  ->FinalHeartData
+)
+
+write.csv( FinalHeartData, file = "Echocardiogram_Cleaned.csv", row.names = FALSE )
