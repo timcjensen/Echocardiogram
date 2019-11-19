8165b0ce29eda6b5e30f0a684b33092a87ac3f11
Justin <36967116+jcx975@users.noreply.github.com>
2019-11-19 08:15:08 -0600

    Fix data

--- data/clean_echocardiogram.R
+++ data/clean_echocardiogram.R
@@ -7,7 +7,7 @@ HeartData <- read.csv( "echocardiogram.csv" )
   HeartData
   %>%select( -c( mult, name, group, alive.at.1 ) )
   %>%filter( !( survival < 12 & still.alive == 1 ) )
-  %>%mutate( target = case_when( survival >= 12 & still.alive == 1 ~ 1, TRUE ~ 0 ) )
+  %>%mutate( target = case_when( survival >= 12 ~ 1, TRUE ~ 0 ) )
   %>%select( -c( survival, still.alive ) )
   ->FinalHeartData
 )
