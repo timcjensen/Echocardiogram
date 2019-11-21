ab0d1efc59a6ade8716ebdfceaebdaf1f2370623
Tim Jensen <timjensen757@gmail.com>
2019-11-21 15:30:56 -0600

    Average feature importance

--- featureselection.py
+++ featureselection.py
@@ -8,6 +8,7 @@ from sklearn.feature_selection import chi2
 from sklearn.ensemble import ExtraTreesClassifier
 import matplotlib.pyplot as plt
 import seaborn as sns
+import numpy as np
 
 X = data.iloc[:,0:7]  #independent columns
 y = data.iloc[:,-1]    #target column i.e price range
@@ -23,12 +24,25 @@ featureScores.columns = ['Features','Score']  #naming the dataframe columns
 print(featureScores.nlargest(7,'Score'))  #print 10 best features
 
 # Feature Importance
-model = ExtraTreesClassifier()
-model.fit(X,y)
-print(model.feature_importances_) #use inbuilt class feature_importances of tree based classifiers
+feature_importance = []
+
+for i in range(250):
+	model = ExtraTreesClassifier()
+	model.fit(X, y)
+	model_feature_importance = model.feature_importances_
+	print(model_feature_importance) #use inbuilt class feature_importances of tree based classifiers
+	feature_importance.append(model_feature_importance)
+
+feature_importance = np.array(feature_importance)
+
 #plot graph of feature importances for better visualization
-feat_importances = pd.Series(model.feature_importances_, index=X.columns)
+avg = np.mean(feature_importance, axis=0)
+feat_importances = pd.Series(avg, index=X.columns)
 feat_importances.nlargest(10).plot(kind='barh')
+plt.xlabel('Importance Score')
+plt.ylabel('Feature')
+plt.title('Feature Importance')
+
 plt.show()
 
 # Correlation Matrix with Heatmap
@@ -42,4 +56,5 @@ b, t = plt.ylim() # discover the values for bottom and top
 b += 0.5 # Add 0.5 to the bottom
 t -= 0.5 # Subtract 0.5 from the top
 plt.ylim(b, t) # update the ylim(bottom, top) values
+plt.title('Correlation Matrix')
 plt.show() # ta-da!
