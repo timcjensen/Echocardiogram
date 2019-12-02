ff4aaf88650c82e6b0b6e57577f591ea1b7a1c09
Tim Jensen <timjensen757@gmail.com>
2019-12-02 09:34:37 -0600

    Return target value

--- mlp_driver.py
+++ mlp_driver.py
@@ -5,11 +5,11 @@ import sys
 clf = load('mlp.joblib')
 
 def predict(age, lvdd, wms):
-	prob = clf.predict_proba([[age, lvdd, wms]])
+	prob = clf.predict([[age, lvdd, wms]])
 	return prob
 
 age = float(sys.argv[1])
 lvdd = float(sys.argv[2])
 wms = float(sys.argv[3])
 
-print(predict(age, lvdd, wms)[0,0])
+print(predict(age, lvdd, wms)[0])
