ffc42d6eb2bbba4c04a4cc64ac662ef60c795ec9
Tim Jensen <timjensen757@gmail.com>
2019-12-03 13:14:54 -0600

    Add testing to MLP

--- mlp_classifier.py
+++ mlp_classifier.py
@@ -22,14 +22,36 @@ scaler = StandardScaler()
 scaler.fit(X)
 X = scaler.transform(X)
 
+tp1 = 0
+fp1 = 0
+tn1 = 0
+fn1 = 0
+
 #10-fold cross validation
 kf = KFold(n_splits=10)
 for train, test in kf.split(data):
 	clf.fit(X[train], y[train])
 	predictions = clf.predict(X[test])
 	print(clf.score(X[test], y[test]))
-	print(confusion_matrix(y[test], predictions))
+	conf_matrix = confusion_matrix(y[test], predictions, labels=[0,1])
+	print(conf_matrix)
+
 	print(classification_report(y[test], predictions))
+
+	tn, fp, fn, tp = conf_matrix.ravel()
+	tn1 += tn
+	fp1 += fp
+	fn1 += fn
+	tp1 += tp
+
+testing_set = np.loadtxt(r'data/testing_set.csv', delimiter=',')
+
+predictions = clf.predict(testing_set[:, 0:3])
+print(clf.score(testing_set[:, 0:3], testing_set[:,-1]))
+conf_matrix = confusion_matrix(testing_set[:,-1], predictions, labels=[0, 1])
+print(conf_matrix)
+
+print(classification_report(testing_set[:,-1], predictions))
 
 # dump trained model to file
 dump(clf, 'mlp.joblib')
